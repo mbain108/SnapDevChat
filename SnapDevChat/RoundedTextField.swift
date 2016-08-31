0e73a97d3f234ec762a4c70ee6a946de6f18a6b1
Melissa Bain <apps@melissadbain.com>
2016-08-31 15:21:33 -0500

    Custom views with IBDesignable and IBInspectable completed

---
+++ SnapDevChat/RoundedTextField.swift
@@ -0,0 +1,53 @@
+//
+//  RoundedTextField.swift
+//  SnapDevChat
+//
+//  Created by Melissa Bain on 8/31/16.
+//  Copyright © 2016 MB Consulting. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class RoundedTextField: UITextField {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        
+        didSet {
+            layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+    
+    @IBInspectable var bgColor: UIColor? {
+        
+        didSet {
+            backgroundColor = bgColor
+        }
+    }
+    
+    @IBInspectable var placeholderColor: UIColor? {
+        
+        didSet {
+            let rawString = attributedPlaceholder?.string != nil ? attributedPlaceholder!.string : ""
+            
+            let str = NSAttributedString(string: rawString, attributes: [NSForegroundColorAttributeName: placeholderColor!])
+            
+            attributedPlaceholder = str
+        }
+    }
+    
+}
