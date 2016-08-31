be140ba0ae67afc0999470d3d143c73cd914949c
Melissa Bain <apps@melissadbain.com>
2016-08-31 11:54:15 -0500

    Refactor Apple's code and move IBOutlets

--- SnapDevChat/AAPLCameraViewController.h
+++ SnapDevChat/AAPLCameraViewController.h
@@ -8,15 +8,16 @@ View controller for camera interface.
 
 
 @import UIKit;
+
 @class AAPLPreviewView;
 @protocol AAPLCameraVCDelegate;
 
 @interface AAPLCameraViewController : UIViewController
+
 @property (nonatomic, weak) AAPLPreviewView *_previewView;
 @property (retain) id <AAPLCameraVCDelegate> delegate;
 
 - (void)changeCamera;
 - (void)toggleMovieRecording;
-
 
 @end
--- SnapDevChat/CameraVC.swift
+++ SnapDevChat/CameraVC.swift
@@ -10,16 +10,13 @@ import UIKit
 
 class CameraVC: AAPLCameraViewController {
 
+    @IBOutlet weak var previewView: AAPLPreviewView!
+    
     override func viewDidLoad() {
+        
+        self._previewView = previewView
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
