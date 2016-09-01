6544b6833d1b1f815317299c5c3904cbf9477c88
Melissa Bain <apps@melissadbain.com>
2016-09-01 11:26:44 -0500

    Creating email account users with Firebase

---
+++ SnapDevChat/AuthService.swift
@@ -0,0 +1,53 @@
+//
+//  AuthService.swift
+//  SnapDevChat
+//
+//  Created by Melissa Bain on 9/1/16.
+//  Copyright © 2016 MB Consulting. All rights reserved.
+//
+
+import Foundation
+import FirebaseAuth
+
+class AuthService {
+    
+    private static let _instance = AuthService()
+    
+    static var instance: AuthService {
+        
+        return _instance
+    }
+    
+    func login(email: String, password: String) {
+        
+        FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, error) in
+            
+            if error != nil {
+                if let errorCode = FIRAuthErrorCode(rawValue: error!._code) {
+                    if errorCode == .errorCodeUserNotFound {
+                        FIRAuth.auth()?.createUser(withEmail: email, password: password, completion: { (user, error) in
+                            if error != nil {
+                                // Show error to user
+                            } else {
+                                if user?.uid != nil {
+                                    // Sign-in
+                                    FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user, error) in
+                                        if error != nil {
+                                            // Show error to user
+                                        } else {
+                                            // We have successfully logged in
+                                        }
+                                    })
+                                }
+                            }
+                        })
+                    }
+                } else {
+                    // Handle all other errors
+                }
+            } else {
+                // Successfully logged in
+            }
+        })
+    }
+}
--- SnapDevChat/LoginVC.swift
+++ SnapDevChat/LoginVC.swift
@@ -9,27 +9,29 @@
 import UIKit
 
 class LoginVC: UIViewController {
+    
+    @IBOutlet weak var emailField: RoundedTextField!
+    @IBOutlet weak var passwordField: RoundedTextField!
 
     override func viewDidLoad() {
+     
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+     
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func loginButtonPressed(_ sender: AnyObject) {
+        
+        if let email = emailField.text, let pass = passwordField.text , (email.characters.count > 0 && pass.characters.count > 0) {
+            
+            AuthService.instance.login(email: email, password: pass)
+        } else {
+            let alert = UIAlertController(title: "Username and Password Required", message: "You must enter both a username and a password", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+            
+            present(alert, animated: true, completion: nil)
+            
+        }
     }
-    */
-
 }
